a9d133eb59156e2bed344e8e0604694a0390896d
johiny <johiny7@live.com>
2021-11-30 17:36:22 -0500

    pagina de landing añadida al router

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
                 <Route path='' element={<Index />} />
                 <Route path='/usuarios' element={<IndexUsuarios />} />
                 <Route path='/usuarios/editar/:_id' element={<EditarUsuario />} />
-                <Route path='landing' element={<LandingPage />} />
+                <Route path='/landing' element={<LandingPage />} />
                 <Route path='category1' element={<IndexCategory1 />} />
                 <Route path='category1/page1' element={<Category1 />} />
               </Route>
