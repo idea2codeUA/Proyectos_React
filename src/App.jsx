ed9d29ab1c8fa27a72b0b6d06b1600e0e7845f27
johiny <johiny7@live.com>
2021-11-30 15:57:42 -0500

    arreglo de conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,6 @@ function App() {
   }, [authToken]);
 
   return (
-<<<<<<< HEAD
     <ApolloProvider client={client}>
       <AuthContext.Provider value={{ authToken, setAuthToken, setToken }}>
         <UserContext.Provider value={{ userData, setUserData }}>
@@ -96,28 +95,6 @@ function App() {
         </UserContext.Provider>
       </AuthContext.Provider>
     </ApolloProvider>
-=======
-    <Auth0Provider
-      domain='misiontic-concesionario.us.auth0.com'
-      clientId='WsdhjjQzDLIZEHA6ouuxXGxFONFGAQ4g'
-      redirectUri='http://localhost:3000/admin'
-      audience='api-autenticacion-concesionario-mintic'
-    >
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path ="landing" element={<LandingPage/>}/>
-            <Route path='/' element={<PrivateLayout />}>
-              <Route path='' element={<Index />} />
-              <Route path='page2' element={<Page2 />} />
-              <Route path='category1' element={<IndexCategory1 />} />
-              <Route path='category1/page1' element={<Category1 />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </Auth0Provider>
->>>>>>> dev_Johiny
   );
 }
 
