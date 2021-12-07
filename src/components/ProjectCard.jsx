6bda7d92c7f2e91686d530f0acf595057e89ed2c
Johiny <johiny7@live.com>
2021-12-07 16:02:58 -0500

    actualizacion de aprobacion del proyecto funcionando

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import "styles/JohinyStyles.css";
 import { EDIT_PROYECTO } from 'graphql/proyectos/mutations';
 import { useMutation } from '@apollo/client';
 
 const MiniCard = (props) => {
 
+    //color predeterminado de las minicards
     let border_color  = "border-blue-500"
-    if(props.item == "PENDIENTE" || props.item == "INACTIVO" )
+    let bg_color = "bg-white"
+    //variantes minicards
+
+    //Estado Pendiente
+    if(props.item == "PENDIENTE")
+    {
+     border_color = "border-yellow-500";
+    }
+    if(props.item == "NO_APROBADO" || props.item == "INACTIVO" )
     {
      border_color = "border-red-500";
+     bg_color = "bg-red-500";
     }
-    if(props.campo == "Aprobación")
+    if(props.item == "APROBADO" || props.item == "ACTIVO" )
+    {
+     border_color = "border-green-500";
+     bg_color = "bg-green-500";
+    }
+    if(props.campo == "Aprobación" || props.campo =="Estado")
     {
     return(
         <div className="cursor-pointer">
         <label className="flex text-center justify-center self-center projectCardsFont font-bold">{props.campo}</label>
-        <div className={`border-2 ${border_color} m-1 p-1 rounded-md shadow-xl hover:bg-red-500`} onClick={props.openModal}>
+        <div className={`border-2 ${border_color} m-1 p-1 rounded-md shadow-xl hover:bg-blue-500 hover:border-blue-500 ${bg_color}`} onClick={() => {props.openModal()
+        props.setProyectSelected(true)}}>
             <span>{props.item}</span>
         </div>
         </div>
@@ -35,11 +51,20 @@ const MiniCard = (props) => {
 
 const ProjectCard = (props) => {
 
+    //mutacion backend
     const [editEstadoProyecto,{data,loading,error}] = useMutation(EDIT_PROYECTO);
 
+    //estado del campo aprobado
+    const [aprobadoState,setAprobadoState] = useState(props.proyecto.aprobado);
+    //estado del campo de del estado del proyecto
+    // estado proyecto seleccionado?
+    const [proyectselected,setProyectSelected] = useState(false);
+
+    //efectos del modal de aprobacion
     useEffect(() => {
-        if(props.acceptTrigger == true)
+        if(props.acceptTrigger == true && proyectselected == true)
         {
+            setAprobadoState("APROBADO");
         editEstadoProyecto({
             variables:{
                 id: props.proyecto._id, 
@@ -54,14 +79,40 @@ const ProjectCard = (props) => {
             }
         })
         props.setAcceptTrigger(false);
+        setProyectSelected(false);
     }
     },[props.acceptTrigger])
+
+    useEffect(() => {
+        if(props.cancelTrigger == true && proyectselected == true )
+        {
+        setAprobadoState("NO_APROBADO");
+        editEstadoProyecto({
+            variables:{
+                id: props.proyecto._id, 
+                nombre: props.proyecto.nombre, 
+                presupuesto: props.proyecto.presupuesto, 
+                fechaInicio: props.proyecto.fechaInicio, 
+                fechaFin: props.proyecto.fechaFin,
+                aprobado: "NO_APROBADO", 
+                estado: props.proyecto.estado, 
+                fase: props.proyecto.fase, 
+                lider: props.proyecto.lider._id
+            }
+        })
+        props.setCancelTrigger(false);
+        setProyectSelected(false);
+    }
+    },[props.cancelTrigger])
+
+    //efectos del modal de estado
+
 
     return(
         <div className="border-2 border-blue-500 p-5 m-10 rounded-xl shadow-xl max-w-2xl">
             <h1 className="text-center align-top text-2xl  projectCardsFont">{props.proyecto.nombre}</h1>
             <div className="flex flex-wrap justify-center">
-            <MiniCard item={props.proyecto.aprobado} campo={"Aprobación"} openModal={props.openModal}/>
+            <MiniCard item={aprobadoState} campo={"Aprobación"} openModal={props.openModal} setProyectSelected={setProyectSelected}/>
             <MiniCard item={props.proyecto.estado} campo={"Estado"}/>
             <MiniCard item={props.proyecto.fase} campo={"Fase"}/>
             <br></br>
--- src/pages/proyectos/IndexProjectos.jsx
+++ src/pages/proyectos/IndexProjectos.jsx
@@ -12,12 +12,23 @@ const IndexProjectos = () => {
     //data del usario logueado
     const {userData} = useUser();
 
-    //estados del modal
+    //Actualizar infromacion despues de cambiarla
+
+    //estados del modal de Aprobacion
     const [open,setOpen] = useState(false);
     const closeModal = () => setOpen(false);
     const openModal = () => setOpen(true);
     const [cancelTrigger,setCancelTrigger] = useState(false);
     const [acceptTrigger,setAcceptTrigger] = useState(false);
+
+    //estados del modal de Estado
+    const [open2,setOpen2] = useState(false);
+    const closeModal2 = () => setOpen(false);
+    const openModal2 = () => setOpen(true);
+    const [cancelTrigger2,setCancelTrigger2] = useState(false);
+    const [acceptTrigger2,setAcceptTrigger2] = useState(false);
+
+
     if (loading) return <div>Cargando....</div>;
 
     if(userData.rol == "ADMINISTRADOR")
@@ -26,11 +37,15 @@ const IndexProjectos = () => {
         <div className="flex flex-wrap justify-center">
         {data.Proyectos.map((proyecto) => {
             return(
-            <ProjectCard key={proyecto._id} proyecto={proyecto} openModal={openModal} acceptTrigger={acceptTrigger} setAcceptTrigger={setAcceptTrigger}/>
+            <ProjectCard key={proyecto._id} proyecto={proyecto} openModal={openModal} openModal2={openModal2} 
+            acceptTrigger={acceptTrigger} setAcceptTrigger={setAcceptTrigger} cancelTrigger={cancelTrigger} setCancelTrigger={setCancelTrigger}
+            acceptTrigger2={acceptTrigger} setAcceptTrigger2={setAcceptTrigger} cancelTrigger2={cancelTrigger} setCancelTrigger2={setCancelTrigger}/>
             );
         })}
-        <ModalJ open={open} closeModal={closeModal} titulo={"¿Quieres aprobar este proyecto?"}
-         textbutton1="Aprobar" textbutton2="No Aprobar" setCancelTrigger={setCancelTrigger} setAcceptTrigger={setAcceptTrigger}/>    
+        <ModalJ open={open} closeModal={closeModal} titulo={"¿Que quieres hacer con este proyecto?"}
+         textbutton1="Aprobar" textbutton2="No Aprobar" setCancelTrigger={setCancelTrigger} setAcceptTrigger={setAcceptTrigger}/>
+         <ModalJ open={open2} closeModal={closeModal2} titulo={"¿Que quieres hacer con este proyecto?"}
+         textbutton1="Activar" textbutton2="No Aprobar" setCancelTrigger={setCancelTrigger2} setAcceptTrigger={setAcceptTrigger2}/>   
         </div>
     )
     }
