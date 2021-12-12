413da46d29ba61bc1b0cf3dff5e246bbdeeb11a1
Johiny <johiny7@live.com>
2021-12-12 18:02:35 -0500

    mejora de la pagina editar usuario

--- src/pages/usuarios/EditarUsuario.jsx
+++ src/pages/usuarios/EditarUsuario.jsx
@@ -56,7 +56,7 @@ const EditarUsuario = () => {
       <Link to='/app/usuarios'>
         <i className='fas fa-arrow-left text-gray-600 cursor-pointer font-bold text-xl hover:text-gray-900' />
       </Link>
-      <h1 className='m-4 text-3xl text-gray-800 font-bold text-center'>Editar Perfil</h1>
+      <h1 className='m-4 text-3xl text-gray-800 font-bold text-center'>{`Editar los datos de ${queryData.Usuario.nombre} ${queryData.Usuario.apellido} `}</h1>
       <form
         onSubmit={submitForm}
         onChange={updateFormData}
