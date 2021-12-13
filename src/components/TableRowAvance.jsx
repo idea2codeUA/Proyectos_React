9f80b289075def36ec4f1f40f2c791a242557cb6
Johiny <johiny7@live.com>
2021-12-12 21:32:55 -0500

    listados de avances por proyectos funcionando

--- src/components/TableRowAvance.jsx
+++ src/components/TableRowAvance.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { nanoid } from 'nanoid'
 const TableRowAvance = (props) => {
     return (
         <tr>
-            <td>Descripción</td>
-            <td>Fecha</td>
-            <td>Creado Por</td>
-            <td>Observaciones</td>
-            <td>
-            <Link to={`editaravance/${props.user._id}`}>
-                <i className='fas fa-pencil-alt text-yellow-600 hover:text-yellow-400 cursor-pointer' />
-            </Link>
-            </td>
+            <td>{props.avance.descripcion}</td>
+            <td>{props.avance.fecha}</td>
+            <td>{`${props.avance.creadoPor.nombre} ${props.avance.creadoPor.apellido}`}</td>
+            <td>{props.avance.observaciones}</td>
+            <td><button className='fas fa-pencil-alt text-gray-900 cursor-pointer bg-yellow-500 hover:bg-yellow-700 hover:text-white p-3 px-6 w-full font-bold'></button></td>
         </tr>      
     )
 }
 
 export default TableRowAvance;
-
-/*
-<td><DropdownJ user={props.user} options={props.options} defaultValue={props.user.estado} 
-name={"estado"} openModal={props.openModal} open = {props.open} closeModal = {props.closeModal}
-cancelTrigger={props.cancelTrigger} setCancelTrigger={props.setCancelTrigger}
-acceptTrigger={props.acceptTrigger} setAcceptTrigger={props.setAcceptTrigger} modalTitulo={props.modalTitulo}
-backendAction={props.backendAction}
-className="border-2 border-blue-500 p-1 px-7"/></td>
-*/
--- src/pages/avances/Avances.jsx
+++ src/pages/avances/Avances.jsx
@@ -1,53 +1,37 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_USUARIOS } from 'graphql/usuarios/queries';
-import {EDITAR_USUARIO} from "graphql/usuarios/mutations";
-import { GET_ESTUDIANTES } from 'graphql/usuarios/queries';
-import { GET_PROYECTOS } from 'graphql/proyectos/queries';
 import { CREAR_AVANCE } from 'graphql/Avances/mutations';
 import { EDITAR_AVANCE } from 'graphql/Avances/mutations';
 import { GET_AVANCES_PROYECTO } from 'graphql/Avances/queries';
 import { toast } from 'react-toastify';
-import { Link } from 'react-router-dom';
 import { Enum_Rol, Enum_EstadoUsuario } from 'utils/enums';
 import PrivateRoute from 'components/PrivateRoute';
 import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import ModalJ from 'components/ModalJ';
-import TableRowJ from 'components/TableRowJ';
 import TableRowAvance from 'components/TableRowAvance';
-import { useMutation } from '@apollo/client';
 import { useUser } from 'context/userContext';
 import { useParams } from 'react-router-dom'
 
 const Avances = () => {
 
-
+  let queryData = null;
   // utiliza los parametros de la url para obtner el id del proyecto
   const {_id} = useParams()
 
   //data del usuario
   const {userData} = useUser();
 
-  //decide que query hacer segun el rol del usuario
-  let QUERY = null;
-  let QUERY1 = null;
-  let queryData = [];
-  let queryData1 = [];
-  const Enum_NO_Autorizado = {NO_AUTORIZADO: 'No autorizado'}
-  const Enum_Autorizado = {AUTORIZADO: 'autorizado'}
-  QUERY = userData.rol == "LIDER" ? GET_USUARIOS : GET_ESTUDIANTES ;
-  QUERY1 = userData.rol == "ESTUDIANTE" ? GET_PROYECTOS : GET_PROYECTOS ;
+  //trae los avances del backend
+  const {data,loading,error} = useQuery(GET_AVANCES_PROYECTO,{
+    variables:{
+      idProyecto: _id 
+    }
+  });
 
-  // trae todos los usuarios o solo los estudiantes
-  const { data, error, loading } = useQuery(QUERY);
-
-  // crea la funcion que se quiera ejecutar en el backend en algun moemnto dado
-  const [editarEstadoUsuario, { data: mutationData, loading: mutationLoading, error: mutationError }] =
-    useMutation(EDITAR_USUARIO);
-
-  // Proyectos por estudiante
-  const {data1,error1,loading1} = useQuery(QUERY1);
+  // pone los datos en una variable mas manejable
+  if(data)
+  {
+    queryData = data.filtrarAvance;
+  }
 
   //estados del modal
     const [open,setOpen] = useState(false);
@@ -56,30 +40,12 @@ const Avances = () => {
     const [cancelTrigger,setCancelTrigger] = useState(false);
     const [acceptTrigger,setAcceptTrigger] = useState(false);
 
-  useEffect(() => {
-    if (error) {
-      toast.error('Error consultando los usuarios');
-    }
-  }, [error]);
-
   if (loading) return <div>Cargando....</div>;
-  else
-  { 
-    //mete la data del query en una variable mas manejable
-    queryData = data[Object.keys(data)[0]];
-    queryData1 = data[Object.keys(data)[0]];
-      // borra la opcion no autorizado del dropdown y filtra de la data los usuarios no autorizados
-      if(userData.rol == "LIDER")  
-      {
-        delete Enum_EstadoUsuario.NO_AUTORIZADO;
-      }
 
-      //Pendiente eliminar estudiante, solo es para pruebas de visualización
+      if(data)
+      {
       return (
-        <PrivateRoute roleList={['ADMINISTRADOR',"LIDER", "ESTUDIANTE"]}> 
-          
-          <div>
-            <button type="submit" className='fas fa-file-upload text-yellow-600 hover:text-yellow-400 cursor-pointer'>Crear Avance</button>
+          <div className='flex flex-col'>
             <table className='tabla'>
               <thead>
                 <tr>
@@ -87,54 +53,22 @@ const Avances = () => {
                   <th>Fecha</th>
                   <th>Creado Por</th>
                   <th>Observaciones</th>
-                  <th>Editar Avance</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
-                {
-                    queryData.map((u) => {
-                      if(userData.rol == "LIDER" && u.estado == "NO_AUTORIZADO")
-                      {
-                        return (
-                          <TableRowAvance key={u._id} user={u} options={Enum_NO_Autorizado} Enum_Rol={Enum_Rol}
-                          cancelTrigger={cancelTrigger} setCancelTrigger={setCancelTrigger} acceptTrigger={acceptTrigger} setAcceptTrigger={setAcceptTrigger}
-                           open={open} openModal={openModal} closeModal={closeModal} backendAction={editarEstadoUsuario} />
-                        );
-                      }
-                      else if (userData.rol == "LIDER" && u.estado == "AUTORIZADO"){
-                        return (
-                          <TableRowAvance key={u._id} user={u} options={Enum_Autorizado} Enum_Rol={Enum_Rol}
-                          cancelTrigger={cancelTrigger} setCancelTrigger={setCancelTrigger} acceptTrigger={acceptTrigger} setAcceptTrigger={setAcceptTrigger}
-                           open={open} openModal={openModal} closeModal={closeModal} backendAction={editarEstadoUsuario} />
-                        );
-                      }
-                      else{
-                        return(
-                        <TableRowAvance key={u._id} user={u} options={Enum_EstadoUsuario} Enum_Rol={Enum_Rol}
-                          cancelTrigger={cancelTrigger} setCancelTrigger={setCancelTrigger} acceptTrigger={acceptTrigger} setAcceptTrigger={setAcceptTrigger}
-                           open={open} openModal={openModal} closeModal={closeModal} backendAction={editarEstadoUsuario} />
-                          )
-                      }
-                    })}                
+                { queryData.map((avance) => {
+                  return(
+                  <TableRowAvance avance={avance}/>
+                  )
+                  })} 
               </tbody>
             </table>
-            <ModalJ open={open} closeModal={closeModal} titulo={"¿Autorizas al usuario?"} textbutton1={"Aceptar"} textbutton2={"Cancelar"}
-            setCancelTrigger={setCancelTrigger} setAcceptTrigger ={setAcceptTrigger}/>
-            
+            <button className=' text-white cursor-pointer max-w-xs bg-blue-600 self-end p-3 mt-1 mr-3 font-bold hover:bg-blue-700 hover:border-blue-600 rounded-lg'>Crear Avance</button>
           </div>
-        </PrivateRoute>
       );
     };
-  }
+  }  
 
 
 export default Avances;
-
-
-/*import React from 'react';
-
-const Category1 = () => {
-  return <div>Avances</div>;
-};
-
-export default Category1;*/
