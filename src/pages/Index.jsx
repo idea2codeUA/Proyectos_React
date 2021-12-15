f64c1b130a57a2e67c4b5c5e573839c2c4b2ecac
David Trujillo <81768830+Dtrujillo-d@users.noreply.github.com>
2021-12-15 01:29:15 -0500

    Modificación al index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 const Index = () => {
   return (
     <div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
+      <div className='bg-gray-200 items-center flex flex-col'>Bienvenido a maneja tu proyecto aqui podras aportar a proyectos existentes o iniciar tus propios proyectos</div>
     </div>
   );
 };
